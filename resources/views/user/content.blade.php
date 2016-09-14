c812cc233e1debce26f2abe20de729397115d093
wambarroto <wambarroto@gmail.com>
2016-09-14 08:36:03 +0700

    new

--- resources/views/user/content.blade.php
+++ resources/views/user/content.blade.php
@@ -1,45 +1,45 @@
         <section class="content">
           <div class="row">
-        <div class="col-xs-12">
-          <div class="box">
-            <div class="box-header">
-              <h3 class="box-title">User</h3>
+            <div class="col-xs-12">
+              <div class="box">
+                <div class="box-header">
+                  <h3 class="box-title">User</h3>
 
-              <div class="box-tools">
-                <a href="{{ action('UserController@create')}}"><button id="add" type="button" class="btn btn btn-success btn-sm">Insert</button></a>
+                  <div class="box-tools">
+                    <a href="{{ action('UserController@create')}}"><button id="add" type="button" class="btn btn btn-success btn-sm">Insert</button></a>
+                  </div>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body table-responsive no-padding">
+                  <table class="table table-hover">
+                    <tr>
+                      <th>ID</th>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Menu</th>
+                    </tr>
+                     @foreach($users as $user)
+                    <tr>
+                      <td>{{ $user->user_id}}</td>
+                      <td>{{ $user->nama}}</td>
+                      <td>{{ $user->email}}</td>
+                      <td>
+                        <a href="user/{{$user->user_id}}/edit"><button type="button" class="btn btn btn-primary btn-sm">Edit</button></a>
+                        <form style="margin-left: 50px; margin-top: -30px;" action="/admin/public/app/user/{{$user->user_id}}" method="post">
+                          <input type ="hidden" name="_method" value="delete">                      
+                          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                          <button type="submit" class="btn btn btn-danger btn-sm" value="delete">Delete</button>
+                        </form>
+                      </td>
+                    </tr>
+                    @endforeach
+
+                  </table>
+                </div>
+                <!-- /.box-body -->
               </div>
+              <!-- /.box -->
             </div>
-            <!-- /.box-header -->
-            <div class="box-body table-responsive no-padding">
-              <table class="table table-hover">
-                <tr>
-                  <th>ID</th>
-                  <th>Name</th>
-                  <th>Email</th>
-                  <th>Menu</th>
-                </tr>
-                 @foreach($users as $user)
-                <tr>
-                  <td>{{ $user->user_id}}</td>
-                  <td>{{ $user->nama}}</td>
-                  <td>{{ $user->email}}</td>
-                  <td>
-                    <a href="app/user/edit"><button type="button" class="btn btn btn-primary btn-sm">Edit</button></a>
-                    <button type="button" class="btn btn btn-danger btn-sm">Delete</button>
-                  </td>
-                </tr>
-                @endforeach
-
-              </table>
-            </div>
-            <!-- /.box-body -->
           </div>
-          <!-- /.box -->
-        </div>
-      </div>
-        </section>  
-
-        <script src="/javascripts/application.js" type="text/javascript" charset="utf-8" async defer>
-        
-        </script>
+        </section>
           
--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -5,11 +5,45 @@
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-      
-    </section>
-    @include('user.edit-user')
-    
-</div>
+		<div class="register-box">
+		  <div class="register-logo">
+		    <a href="../../index2.html"><b>Admin</b>LTE</a>
+		  </div>
 
+		  <div class="register-box-body">
+		    <p class="login-box-msg">Edit membership</p>
+
+		    <form action="/admin/public/app/user/{{$user->user_id}}" method="post">
+		      <div class="form-group has-feedback">
+		        <input type="text" class="form-control" name="nama" value="{{$user->nama}}" placeholder="nama">
+		        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+		      </div>
+		      <div class="form-group has-feedback">
+		        <input type="text" class="form-control" name="username" value="{{$user->username}}" placeholder="username">
+		        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+		      </div>
+		      <div class="form-group has-feedback">
+		        <input type="email" class="form-control" name="email" value="{{$user->email}}" placeholder="email">
+		        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+		      </div>
+		      <div class="form-group has-feedback">
+		        <input type="password" class="form-control" name="password" value="{{$user->password}}" placeholder="password">
+		        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+		      </div>
+		      <div class="row">
+		        <div class="col-xs-4">
+		          <input type="hidden" name="_method" value="put">
+		          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+		          <button type="submit" value="edit" class="btn btn-block btn-primary">Submit</button>
+		        </div>
+		        <!-- /.col -->
+		      </div>
+		    </form>
+		  </div>
+		  <!-- /.form-box -->
+		</div>
+		<!-- /.register-box -->
+    </section>    
+</div>
   @include('footer')
   @include('js')
